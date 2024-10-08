da7fead3413f5994538bf15ef61fd0d5287f0e81
Borse030 <109461933+Borse030@users.noreply.github.com>
2024-10-08 16:22:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Product_Details from './Components/Product_Details/Product_Details';
+import ProductDetails  from './Components/Product_Details/Product_Details';
 import Cart from './Components/Cart/Cart';
 import Error from './Components/Error/Error';
-import Single_Product from './Components/Single_Product/Single_Product';
+import SingleProduct from './Components/Single_Product/Single_Product';
 import 'react-toastify/dist/ReactToastify.css'; // Import Toastify CSS
 
 import { ToastContainer } from 'react-toastify';
@@ -15,8 +15,8 @@ function App() {
     <BrowserRouter>
     <ToastContainer />
     <Routes>
-    <Route path="/" element={<Product_Details/>} />
-    <Route path="/Single_Product/:id" element={<Single_Product/>} />
+    <Route path="/" element={<ProductDetails/>} />
+    <Route path="/SingleProduct/:id" element={<SingleProduct/>} />
     <Route path="/Cart" element={<Cart/>} />
     <Route path="/*" element={<Error/>} />
 
